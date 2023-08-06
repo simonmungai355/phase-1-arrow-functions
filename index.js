e786f4647a23409dc85f5665f815367028830a4d
simonmungai355 <simon.mungai1@student.moringaschool.com>
2023-08-06 19:35:08 +0300

    complete

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1 , num2) => 2000 / 100;
+
+    //const (num1 , num2) = () => 2000 / 100;
+
+ const square = (num) => num * num;
+
+    //squire = () => 4* 4;
+
+    const add = (x, y) => x + y;
+
+   // add = () => 50 + 150;
+        
